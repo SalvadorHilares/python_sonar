ea65ea46b3475f9bba89aeb3f3468ec125e9077b
Salvador Hilares <salvador.hilares@utec.edu.pe>
2022-07-11 23:23:19 -0500

    Coverage

--- app.py
+++ app.py
@@ -1,5 +1,3 @@
-import unittest
-
 class Usuario():
     def __init__(self, username):
         self.username = username
@@ -11,22 +9,3 @@ class CentroVacunacion():
 
     def personas_vacunadas(self,personas):
         self.total_vacunados += personas
-
-class Tests(unittest.TestCase):
-
-    user = Usuario('pepito')
-    centro_vacunacion = CentroVacunacion()
-
-    def test_login(self):
-        test_password=''.join(reversed(self.user.username))
-        self.assertEqual(self.user.username,'pepito')
-        self.assertEqual(test_password,'otipep')
-    
-    def test_vacunados(self):
-        self.centro_vacunacion.personas_vacunadas(1000)
-        self.centro_vacunacion.personas_vacunadas(1500)
-        self.centro_vacunacion.personas_vacunadas(1200)
-        self.assertEqual(self.centro_vacunacion.total_vacunados,3700)
-
-if __name__ == '__main__':
-    unittest.main()
